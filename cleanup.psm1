d9ed8e50f87b8678e585c6da365ad05de2bd263d
dr ray <51449266+dr-raypc@users.noreply.github.com>
2023-04-21 10:54:51 -0400

    Add files via upload

--- cleanup.psm1
+++ cleanup.psm1
@@ -1,40 +1,63 @@
 function cleanup {
+#	Get System Uptime	
 	$uptime = (get-date) - (gcim Win32_OperatingSystem).LastBootUpTime | select days,hours,minutes
 	Write-Host "`nDevice Uptime:	" -NoNewLine
 	Write-Host "$uptime`n" -ForegroundColor Green
+
+
+#	Install Windows Update	
 	Install-WindowsUpdate -AcceptAll -MicrosoftUpdate
 	Write-Host "[x] " -ForegroundColor Green -NoNewLine
-	Write-Host "Installed Windows Updates.`n"	
-	$winupdatefiles = "C:\Windows\SoftwareDistribution\Download\*"
-	$winupdatesize = "{0:N2} GB" -f ((gci $winupdatefiles -Recurse -ErrorAction SilentlyContinue  | measure Length -s).sum / 1Gb)
+	Write-Host "Installed Windows Updates.`n"
+
+
+#	Get Windows Update download size and remove it	
+	$winupdatefiles = "C:\Windows\SoftwareDistribution\Download"
+#	$winupdatesize = "{0:N2} GB" -f ((Get-ChildItem $winupdatefiles -Recurse -ErrorAction SilentlyContinue | where-object { -not $_.PSIsContainer } | measure Length -s).sum / 1Gb)
+	$winupdatesize = "{0:N2} MB" -f ((Get-ChildItem $winupdatefiles -Recurse | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue).Sum / 1MB)
 	Remove-Item $winupdatefiles -Recurse -Force -ErrorAction SilentlyContinue
 	Write-Host "[x] " -ForegroundColor Green -NoNewLine
 	Write-Host "$winupdatesize Windows Update Files Removed.`n"
+	
+
+#	Get Windows Temp size and remove it
 	$wintempfiles = "C:\Windows\Temp\*"
-	$wintempsize = "{0:N2} GB" -f ((gci $wintempfiles -Recurse -ErrorAction SilentlyContinue  | measure Length -s).sum / 1Gb)
+	$wintempsize = "{0:N2} GB" -f ((Get-ChildItem $wintempfiles -Recurse -ErrorAction SilentlyContinue  | measure Length -s).sum / 1Gb)
 	Remove-Item $wintempfiles -Recurse -Force -ErrorAction SilentlyContinue
 	Write-Host "[x] " -ForegroundColor Green -NoNewLine
 	Write-Host "$wintempsize Windows Temp Files Removed.`n"
-	$windmpfiles = gci c:\ *.dmp -Recurse -Force -ErrorAction SilentlyContinue
-#	$windmpsize = "{0:N2} GB" -f ((gci $windmpfiles -Recurse -ErrorAction SilentlyContinue | measure Length -s).sum / 1Gb)
+
+
+#	Get Windows dump file size and remove them
+	$windmpfiles = Get-ChildItem c:\ *.dmp -Recurse -Force -ErrorAction SilentlyContinue
+#	$windmpsize = "{0:N2} GB" -f ((Get-ChildItem $windmpfiles -Recurse -ErrorAction SilentlyContinue | measure Length -s).sum / 1Gb)
 	$windmpsize = "{0:N2} GB" -f (($windmpfiles | measure Length -s).sum / 1Gb)
 	Remove-Item $windmpfiles -Recurse -Force -ErrorAction SilentlyContinue
 	Write-Host "[x] " -ForegroundColor Green -NoNewLine
 	Write-Host "$windmpsize Windows Dump Files Removed.`n"
+
+
+#	Get User temp file size and remove them
 	#$usertmpfiles = Get-ChildItem -Path 'C:\Users' -ErrorAction SilentlyContinue | foreach {
 	#	Get-ChildItem -Path "$($_.FullName)\AppData\Local\Temp\*" -Recurse -ErrorAction SilentlyContinue
 	#	Get-ChildItem -Path "$($_.FullName)\AppData\Local\Temporary Internet Files\*" -Recurse -ErrorAction SilentlyContinue
 	#}
-	#$usertmpfilesize = "{0:N2} GB" -f ((gci $usertmpfiles -Recurse -ErrorAction SilentlyContinue | measure Length -s).sum / 1Gb)
+	#$usertmpfilesize = "{0:N2} GB" -f ((Get-ChildItem $usertmpfiles -Recurse -ErrorAction SilentlyContinue | measure Length -s).sum / 1Gb)
 	#Remove-Item $usertmpfiles -Recurse -Force -ErrorAction SilentlyContinue
 	#Write-Host "[x] " -ForegroundColor Green -NoNewLine
 	#Write-Host "$usertmpfilesize Temporary User Files Removed.`n"
+
+
+#	Optimize C drive and disable hibernation
 	Optimize-Volume -DriveLetter C -ReTrim -ErrorAction SilentlyContinue
 	Write-Host "[x] " -ForegroundColor Green -NoNewLine
 	Write-Host "Optimized SSD (ReTrim)`n"
 	powercfg -h off
 	Write-Host "[x] " -ForegroundColor Green -NoNewLine
 	Write-Host "Disabled Hibernate Feature`n"
+
+
+#	Clean Windows Component Store
 	dism /online /cleanup-image /analyzecomponentstore
 	dism /online /cleanup-image /startcomponentcleanup
 	dism /online /cleanup-image /startcomponentcleanup /resetbase
