5d9f62d0ac9d9c0d565e4ecbe24399a73b363f30
dr ray <51449266+dr-raypc@users.noreply.github.com>
2023-11-08 11:24:22 -0500

    Add files via upload

--- cleanup.psm1
+++ cleanup.psm1
@@ -1,6 +1,47 @@
+# Author:	Raymond Mayer
+# Updated:	11/8/2023	-	Added SERVER switch.
+
+param(
+    [switch]$server
+  )
 function cleanup {
+	if ($server) {
+		#	Get System Uptime	
+		$uptime = (get-date) - (gcim Win32_OperatingSystem).LastBootUpTime | Select-Object days,hours,minutes
+		Write-Host "`nDevice Uptime:	" -NoNewLine
+		Write-Host "$uptime`n" -ForegroundColor Green
+
+
+		#	Get Windows Update download size and remove it	
+		$winupdatefiles = "C:\Windows\SoftwareDistribution\Download"
+		#	$winupdatesize = "{0:N2} GB" -f ((Get-ChildItem $winupdatefiles -Recurse -ErrorAction SilentlyContinue | where-object { -not $_.PSIsContainer } | measure Length -s).sum / 1Gb)
+		$winupdatesize = "{0:N2} MB" -f ((Get-ChildItem $winupdatefiles -Recurse | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue).Sum / 1MB)
+		Remove-Item $winupdatefiles -Recurse -Force -ErrorAction SilentlyContinue
+		Write-Host "[x] " -ForegroundColor Green -NoNewLine
+		Write-Host "$winupdatesize Windows Update Files Removed.`n"
+
+
+		#	Get Windows Temp size and remove it
+		$wintempfiles = "C:\Windows\Temp\*"
+		$wintempsize = "{0:N2} GB" -f ((Get-ChildItem $wintempfiles -Recurse -ErrorAction SilentlyContinue  | Measure-Object Length -s).sum / 1Gb)
+		Remove-Item $wintempfiles -Recurse -Force -ErrorAction SilentlyContinue
+		Write-Host "[x] " -ForegroundColor Green -NoNewLine
+		Write-Host "$wintempsize Windows Temp Files Removed.`n"
+
+		#	Clean Windows Component Store
+		dism /online /cleanup-image /analyzecomponentstore
+		dism /online /cleanup-image /startcomponentcleanup
+		dism /online /cleanup-image /startcomponentcleanup /resetbase
+		Write-Host "`n`n[x] " -ForegroundColor Green -NoNewLine
+		Write-Host "Cleaned Windows Component Store`n"
+		cleanmgr
+		Write-Host "[x] " -ForegroundColor Green -NoNewLine
+		Write-Host "Executed Final Disk Cleanup`n`n"
+	}
+
+
 #	Get System Uptime	
-	$uptime = (get-date) - (gcim Win32_OperatingSystem).LastBootUpTime | select days,hours,minutes
+	$uptime = (get-date) - (gcim Win32_OperatingSystem).LastBootUpTime | Select-Object days,hours,minutes
 	Write-Host "`nDevice Uptime:	" -NoNewLine
 	Write-Host "$uptime`n" -ForegroundColor Green
 
@@ -15,14 +56,14 @@ function cleanup {
 	$winupdatefiles = "C:\Windows\SoftwareDistribution\Download"
 #	$winupdatesize = "{0:N2} GB" -f ((Get-ChildItem $winupdatefiles -Recurse -ErrorAction SilentlyContinue | where-object { -not $_.PSIsContainer } | measure Length -s).sum / 1Gb)
 	$winupdatesize = "{0:N2} MB" -f ((Get-ChildItem $winupdatefiles -Recurse | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue).Sum / 1MB)
-	Remove-Item $winupdatefiles -Recurse -Force -ErrorAction SilentlyContinue
+	Remove-Item $winupdatefiles -Force -Recurse -ErrorAction SilentlyContinue
 	Write-Host "[x] " -ForegroundColor Green -NoNewLine
 	Write-Host "$winupdatesize Windows Update Files Removed.`n"
 	
 
 #	Get Windows Temp size and remove it
 	$wintempfiles = "C:\Windows\Temp\*"
-	$wintempsize = "{0:N2} GB" -f ((Get-ChildItem $wintempfiles -Recurse -ErrorAction SilentlyContinue  | measure Length -s).sum / 1Gb)
+	$wintempsize = "{0:N2} GB" -f ((Get-ChildItem $wintempfiles -Recurse -ErrorAction SilentlyContinue  | Measure-Object Length -s).sum / 1Gb)
 	Remove-Item $wintempfiles -Recurse -Force -ErrorAction SilentlyContinue
 	Write-Host "[x] " -ForegroundColor Green -NoNewLine
 	Write-Host "$wintempsize Windows Temp Files Removed.`n"
@@ -31,10 +72,18 @@ function cleanup {
 #	Get Windows dump file size and remove them
 	$windmpfiles = Get-ChildItem c:\ *.dmp -Recurse -Force -ErrorAction SilentlyContinue
 #	$windmpsize = "{0:N2} GB" -f ((Get-ChildItem $windmpfiles -Recurse -ErrorAction SilentlyContinue | measure Length -s).sum / 1Gb)
-	$windmpsize = "{0:N2} GB" -f (($windmpfiles | measure Length -s).sum / 1Gb)
+	$windmpsize = "{0:N2} GB" -f (($windmpfiles | Measure-Object Length -s).sum / 1Gb)
 	Remove-Item $windmpfiles -Recurse -Force -ErrorAction SilentlyContinue
 	Write-Host "[x] " -ForegroundColor Green -NoNewLine
 	Write-Host "$windmpsize Windows Dump Files Removed.`n"
+
+
+#	Get User temp file size and remove them	
+	$usertmpfiles = Get-ChildItem -Path "$env:SystemDrive\Users\*\AppData\Local\Temp\*" -Recurse -Force
+	$usertmpfilesize = "{0:N2} GB" -f ((Get-ChildItem $usertmpfiles -Recurse -ErrorAction SilentlyContinue | Measure-Object Length -s).sum / 1Gb)
+	Remove-Item $usertmpfiles -Recurse -Force -ErrorAction SilentlyContinue
+	Write-Host "[x] " -ForegroundColor Green -NoNewLine
+	Write-Host "$usertmpfilesize Temporary User Files Removed.`n"
 
 
 #	Get User temp file size and remove them
